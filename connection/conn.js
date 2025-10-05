af12655dc4d01df307cdd27a02927c003cb49e64
Ohad Raza <ohadraza508@gmail.com>
2025-10-05 20:50:03 +0500

    Fix backend for Vercel deployment

--- connection/conn.js
+++ connection/conn.js
@@ -5,13 +5,9 @@ import dotenv from "dotenv";
 dotenv.config();
 const conn = async (req, res) => {
   try {
-    await mongoose
-      .connect(
-        "mongodb+srv://ohad58444_db_user:JIT6Vrr19vMHs7Rc@cluster0.8njyvwz.mongodb.net/"
-      )
-      .then(() => {
-        console.log("connected to mongoDB");
-      });
+    await mongoose.connect(process.env.MONGO_URI).then(() => {
+      console.log("connected to mongoDB");
+    });
   } catch (error) {
     res.status(200).json({ message: "not connected to DB" });
   }
